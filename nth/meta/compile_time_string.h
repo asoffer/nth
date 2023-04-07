7ce2421f32a353cd0a48bc6c10aef271c1c317d9
asoffer <andy.soffer@gmail.com>
2023-04-07 19:54:33 -0400

    Hide data member of CompileTimeString.

--- nth/meta/compile_time_string.h
+++ nth/meta/compile_time_string.h
@@ -10,18 +10,30 @@ template <size_t Length>
 struct CompileTimeString {
   constexpr CompileTimeString(char const (&buffer)[Length + 1]) {
     for (size_t i = 0; i < Length + 1; ++i) {
-      implementation_data_[i] = buffer[i];
+      NthInternalCompileTimeStringDataMember[i] = buffer[i];
     }
   }
 
-  size_t length() const { return Length; }
-  size_t size() const { return Length; }
+  constexpr char const* data() const {
+    return NthInternalCompileTimeStringDataMember;
+  }
+  constexpr size_t length() const { return Length; }
+  constexpr size_t size() const { return Length; }
 
-  constexpr operator char const*() const { return implementation_data_; }
-  constexpr operator std::string_view() const { return implementation_data_; }
+  constexpr operator char const*() const { return NthInternalCompileTimeStringDataMember; }
+  constexpr operator std::string_view() const { return NthInternalCompileTimeStringDataMember; }
 
-  char implementation_data_[Length + 1];
+  char NthInternalCompileTimeStringDataMember[Length + 1];
 };
+#define NthInternalCompileTimeStringDataMember                                 \
+  data() + [] {                                                                \
+    static_assert(false,                                                       \
+                  "Do not attempt to use "                                     \
+                  "`NthInternalCompileTimeStringDataMember` directly. "        \
+                  "Access the data via the public member function "            \
+                  "`nth::CompileTimeString::data().");                         \
+    return 0;                                                                  \
+  }()
 
 template <size_t N>
 CompileTimeString(char const (&)[N]) -> CompileTimeString<N - 1>;
