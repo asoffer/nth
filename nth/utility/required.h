4e6f9a23a00f109ebaf0f200d3817c86f7781ba3
asoffer <andy.soffer@gmail.com>
2023-05-27 14:35:36 -0400

    It seems Clang-16 does not eagerly instantiate this template.

--- nth/utility/required.h
+++ nth/utility/required.h
@@ -6,31 +6,12 @@
 namespace nth {
 namespace internal_required {
 
-#if (defined(__clang__) and __clang_major__ >= 16) or defined(_MSC_VER)
-#define NTH_INTERNAL_REQUIRED_EAGERLY_INSTANTIATION 0
-#else 
-#define NTH_INTERNAL_REQUIRED_EAGERLY_INSTANTIATION 1
-#endif
-
 struct Impl {
   template <typename T>
-  operator T() const
-#if NTH_INTERNAL_REQUIRED_EAGERLY_INSTANTIATION
-      // On compilers that eagerly instantiate this template, we cannot provide
-      // a definition (lest it be instantiated and therefore invoke the
-      // static assertion. Instead we opt to leave it undefined so that the
-      // linker complains about the missing implementation.
-      ;
-#else
-  // On compilers that do not eagerly instantiate this template, we can provide
-  // a useful diagnostic indicating that field is required.
-  {
+  operator T() const {
     static_assert(::nth::type<T>.dependent(false), "This field is required.");
   }
-#endif
 };
-
-#undef NTH_INTERNAL_REQUIRED_EAGERLY_INSTANTIATION
 
 }  // namespace internal_required
 
