6ba7c8a646745230120aa3bba006012e21e685d4
asoffer <andy.soffer@gmail.com>
2023-07-31 12:23:47 -0400

    Adding a aggreagting header to make it easier to opt-in to all debugging facilities.

---
+++ nth/debug/debug.h
@@ -0,0 +1,9 @@
+#ifndef NTH_DEBUG_DEBUG_H
+#define NTH_DEBUG_DEBUG_H
+
+#include "nth/debug/log/log.h"
+#include "nth/debug/trace/trace.h"
+#include "nth/debug/unimplemented.h"
+#include "nth/debug/unreachable.h"
+
+#endif  // NTH_DEBUG_DEBUG_H
