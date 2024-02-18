40099af8cf1ba53c02db609b7c6c38069353971a
Andy Soffer <andy.soffer@gmail.com>
2024-02-18 05:41:51 -0500

    Add arm64 platform support to jit_function_test.

--- nth/base/platform.h
+++ nth/base/platform.h
@@ -7,27 +7,58 @@
 // platform-independent behavior often requires querying specific platforms, for
 // which this macro can be used.
 //
-// Supported values of the `compiler_name` parameter are: `clang`, `gcc`, and
-// `msvc`.
+// Supported values of the `compiler_name` parameter are: `clang`, `gcc`,
+// `msvc`, and `unknown`.
 #define NTH_COMPILER(compiler_name)                                            \
   NTH_CONCATENATE(NTH_BASE_PLATFORM_INTERNAL_COMPILER_, compiler_name)
+
+// Expands to either `true` or `false` depending on the executing hardware
+// architecture. Hardware detection should be avoided if at all possible,
+// relying instead on platform-independent behaviors. However, implementing such
+// platform-independent behavior often requires querying specific platforms, for
+// which this macro can be used.
+//
+// Supported values of the `anchitecture` parameter are: `arm64`, and `x64`, and
+// `unknown`.
+#define NTH_ARCHITECTURE(anchitecture)                                         \
+  NTH_CONCATENATE(NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_, anchitecture)
+
+// Implementation.
 
 #if defined(__clang__)
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_clang true
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_gcc false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_msvc false
+#define NTH_BASE_PLATFORM_INTERNAL_COMPILER_unknown false
 #elif defined(__GNUC__)
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_clang false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_gcc true
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_msvc false
+#define NTH_BASE_PLATFORM_INTERNAL_COMPILER_unknown false
 #elif defined(_MSC_VER)
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_clang false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_gcc false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_msvc true
+#define NTH_BASE_PLATFORM_INTERNAL_COMPILER_unknown false
 #else
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_clang false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_gcc false
 #define NTH_BASE_PLATFORM_INTERNAL_COMPILER_msvc false
+#define NTH_BASE_PLATFORM_INTERNAL_COMPILER_unknown true
+#endif
+
+#if defined(__aarch64__) or defined(__arm64__)
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_arm64 true
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_x64 false
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_unknown false
+#elif defined(__x86_64__)
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_arm64 false
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_x64 true
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_unknown false
+#else
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_arm64 false
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_x64 false
+#define NTH_BASE_PLATFORM_INTERNAL_ARCHITECTURE_unknown true
 #endif
 
 #endif  // NTH_BASE_PLATFORM_H
--- nth/dynamic/jit_function_test.cc
+++ nth/dynamic/jit_function_test.cc
@@ -1,5 +1,6 @@
 #include "nth/dynamic/jit_function.h"
 
+#include "nth/base/platform.h"
 #include "nth/test/test.h"
 
 namespace nth {
@@ -12,12 +13,21 @@ NTH_TEST("jit_function/default-construction") {
 }
 
 std::span<std::byte const> SquareFunctionCode() {
-  // TODO: Architecture dependence.
-  static constexpr uint8_t buffer[] = {
-      0x0f, 0xaf, 0xff,  // imul edi, edi
-      0x89, 0xf8,        // mov eax, edi
-      0xc3,              // ret
-  };
+  static constexpr uint8_t buffer[] =
+#if NTH_ARCHITECTURE(x64)
+      {
+          0x0f, 0xaf, 0xff,  // imul edi, edi
+          0x89, 0xf8,        // mov eax, edi
+          0xc3,              // ret
+      };
+#elif NTH_ARCHITECTURE(arm64)
+      {
+          0x00, 0x7c, 0x00, 0x1b,  // mul w0, w0, w0
+          0xc0, 0x03, 0x5f, 0xd6,  // ret
+      };
+#else  // NTH_ARCHITECTURE
+#error Unsupported architecture.
+#endif  // NTH_ARCHITECTURE
 
   return std::span<std::byte const>(reinterpret_cast<std::byte const*>(buffer),
                                     sizeof(buffer));
