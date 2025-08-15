000210d69be02088ddd2c3cb00b57ee11a2838d2
Andy Soffer <andy.soffer@gmail.com>
2025-08-14 21:16:20 -0400

    Only write to stderr and stdout log sinks in color.

--- nth/debug/log/file_log_sink.h
+++ nth/debug/log/file_log_sink.h
@@ -10,23 +10,31 @@
 namespace nth {
 
 struct file_log_sink : log_sink {
-  explicit file_log_sink(nth::io::file_writer& w NTH_ATTRIBUTE(lifetimebound))
-      : writer_(w) {}
+  explicit file_log_sink(nth::io::file_writer& w NTH_ATTRIBUTE(lifetimebound),
+                         bool ansi_color = false)
+      : writer_(w), ansi_color_(ansi_color) {}
 
   void send(log_configuration const& config, log_line const& line,
             log_entry const& entry) override {
     auto source_loc = config.source_location().value_or(line.source_location());
-    nth::interpolate<"\x1b[0;36m{}:{} {}]\x1b[0m {}\n">(
-        writer_, source_loc.file_name(), source_loc.line(),
-        source_loc.function_name(), entry);
+    if (ansi_color_) {
+      nth::interpolate<"\x1b[0;36m{}:{} {}]\x1b[0m {}\n">(
+          writer_, source_loc.file_name(), source_loc.line(),
+          source_loc.function_name(), entry);
+    } else {
+      nth::interpolate<"{}:{} {}] {}\n">(writer_, source_loc.file_name(),
+                                         source_loc.line(),
+                                         source_loc.function_name(), entry);
+    }
   }
 
  private:
   nth::io::file_writer& writer_;
+  bool ansi_color_;
 };
 
-inline file_log_sink stderr_log_sink(nth::io::stderr_writer);
-inline file_log_sink stdout_log_sink(nth::io::stdout_writer);
+inline file_log_sink stderr_log_sink(nth::io::stderr_writer, true);
+inline file_log_sink stdout_log_sink(nth::io::stdout_writer, true);
 
 }  // namespace nth
 
